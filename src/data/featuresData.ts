dc2d53a93ea70a10b7b330ca3586ad2608a36d4e
Suryansh <suryansh@Suryanshs-MacBook-Air.local>
2026-05-11 11:32:16 +0530

    changes in feature section and latest news

--- src/data/featuresData.ts
+++ src/data/featuresData.ts
@@ -1,7 +1,4 @@
 
-import collegeImg from "../assets/images/college-5757815_1280.jpg";
-import partnershipImg from "../assets/images/partnership_image.png";
-
 export interface Feature {
   title: string;
   description: string;
@@ -19,13 +16,13 @@ export const features: Feature[] = [
     title: "Salient Features",
     description:
       "To enable students to face the present competitive era and make them aware of various real-world scenarios, the school provides valuable guidance and counselling sessions conducted by experienced experts. These professionals assist students in making realistic educational and vocational choices according to their aptitude and interests.",
-    image: collegeImg,
+    image: "/salient.jpeg",
   },
   {
     title: "Computer Lab",
     description:
       "In this era of information technology , the school has its own grand computer lab equipped with 40 computers with Core 2 Duo Intel Processor, TFT Monitor . The computer lab is equipped with state of art projector and touch – screen control & command box.",
-    image: "/computer.webp",
+    image: "/computerlab.jfif",
   },
   {
     title: "Science Lab",
@@ -37,30 +34,36 @@ export const features: Feature[] = [
     title: "Library",
     description:
       "GVMPS has a spacious library well stocked with about 15,000 books with the number increasing with each passing year. The library has sections ranging from text books and reference books to fiction and general knowledge. There are a variety of books available for students of all levels on various subjects, literature, travel etc. the school subscribes newspapers and magazines to enable students to gain knowledge and develop interest in reading. The students are encouraged to read good books to widen their interests and extend their mental horizon.",
-    image: "/library.webp",
+    image: "/library.jfif",
   },
   {
     title: "Guidance Bureau",
     description:
       "To enable the students to face the present competitive era and make them aware of various scenario in present time, the school is providing valuable guidance-cum-counselling by the area experts , who assist them in marking realistic educational and vocational choices as per their aptitude. The school psychiatrist also helps the students to overcome their personal as well as social problems in a specialized manner.",
-    image: partnershipImg,
+    image: "/guidance .jfif",
   },
   {
     title: "Games & Sports",
     description:
       "The school has a well equipped games and sports section with facilities for various indoor and outdoor games. The students are encouraged to participate in various sports activities to promote physical fitness and team spirit.",
-    image: "/sports.webp",
+    image: "/sports.jfif",
   },
   {
     title: "Music & Dance",
     description:
       "Music and dance room equipped with various musical instruments and dedicated teachers are striving to develop their students as nightingale and rocking stars.",
-    image: "/dance.webp",
+    image: "/music.jfif",
   },
   {
     title: "Arts & Craft",
     description:
       "The well qualified art teachers bring the students close to the life of colors through regular periods and a large variety of co —curricular activities.",
-    image: "/art-craft.webp",
+    image: "/artandcraft.jfif",
+  },
+  {
+    title: "Robotics Lab",
+    description:
+      "A state-of-the-art Robotics Lab where students can explore the world of automation, coding, and building robots, fostering innovation and critical thinking.",
+    image: "/robotics.jfif",
   },
 ];
--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,41 +1,40 @@
-import Admissions from "../assets/images/Admission_image.png";
 import Sciencelab from "../assets/images/sciencelab_image.png";
 
 
 
 export const sliderCards = [
   {
-    img: Sciencelab,
+    img: "/co.jfif",
     tag: "Co-Education",
     text: "Co-Education and Pre-Prep",
     link: "/co-education",
   },
   {
-    img: "https://www.westminster.org.uk/wp-content/uploads/2025/06/250522-Library-03-scaled.jpg",
+    img: "/building.jpeg",
     tag: "Our Future",
     text: "Building for the Future",
     link: "/our-future",
   },
   {
-    img: Admissions,
+    img: "/addmission.jpeg",
     tag: "Admission",
     text: "Admissions",
     link: "/admissions",
   },
   {
-    img: Sciencelab,
+    img: "/co.jfif",
     tag: "Co-Education",
     text: "Co-Education and Pre-Prep",
     link: "/co-education",
   },
   {
-    img: "https://www.westminster.org.uk/wp-content/uploads/2025/06/250522-Library-03-scaled.jpg",
+    img: "/building.jpeg",
     tag: "Our Future",
     text: "Building for the Future",
     link: "/our-future",
   },
   {
-    img: Admissions,
+    img: "/addmission.jpeg",
     tag: "Admission",
     text: "Admissions",
     link: "/admissions",
